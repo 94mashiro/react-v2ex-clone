67220bea4dec76099358b5ae18d3fcc5b0678be6
Mashiro Wang <hyperoot@gmail.com>
2016-10-25 13:45:49 +0800

    fix "<a> node out of navbar" bug

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -6,19 +6,32 @@ import '../Stylesheets/Navbar.css'
 export default class Navbar extends Component {
   render() {
     return (
-      <div>
-        <nav className="navbar navbar-default" role="navigation">
-          <div className="container-fluid">
-            <div className="navbar-header">
-              <Link className="navbar-brand" to="/"><img src="https://cdn.v2ex.co/site/logo@2x.png" width="94" height="30"/></Link>
-            </div>
-              <ul className="nav navbar-nav navbar-right nav-right-btn">
-                <a href="#">首页</a>
-                <a href="#">设置</a>
-                <a href="#">登出</a>
-              </ul>
+      <div className="top-navbar">
+        <div className="content">
+          <div style={{paddingTop:'6px'}}>
+            <table width="100%">
+              <tbody>
+                <tr>
+                  <td width='110px' align='left'>
+                    <Link to='/'>
+                      <img src="https://cdn.v2ex.co/site/logo@2x.png" width="94" height="30"/>
+                    </Link>
+                  </td>
+                  <td width="auto" align="center" style={{paddingTop: '2px'}}>
+
+                  </td>
+                  <td width="200px" align="right" style={{paddingTop: '2px'}}>
+                    <Link to="/">首页</Link>
+                    &nbsp;&nbsp;&nbsp;
+                    <Link to="/">设置</Link>
+                    &nbsp;&nbsp;&nbsp;
+                    <Link to="/">登出</Link>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
           </div>
-        </nav>
+        </div>
       </div>
     );
   }
